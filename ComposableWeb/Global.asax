afeae42d10ee86e150a68e25917324e6de0fde08
user <user@WIN10-AUG2016>
2016-10-23 14:29:09 +0100

    Add project files.

---
+++ ComposableWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ComposableWeb.MvcApplication" Language="C#" %>
---
+++ ComposableWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ComposableWeb.Startup))]
+namespace ComposableWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
